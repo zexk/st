00e9dcdded5d050951739a08a745ba3e28a10d27
Zexk <bouraoui@outlook.it>
2024-12-03 09:11:09 +0100

    colorscheme added

--- config.def.h
+++ config.def.h
@@ -95,33 +95,29 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+  /* 8 normal colors */
+  [0] = "#261d37", /* black   */
+  [1] = "#5b4c7d", /* red     */
+  [2] = "#7a69a0", /* green   */
+  [3] = "#9887be", /* yellow  */
+  [4] = "#b6a7d6", /* blue    */
+  [5] = "#d3c8ea", /* magenta */
+  [6] = "#c9929a", /* cyan    */
+  [7] = "#ba9e80", /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 bright colors */
+  [8]  = "#c9929a", /* black   */
+  [9]  = "#ba9e80", /* red     */
+  [10] = "#c1c07f", /* green   */
+  [11] = "#9eae7b", /* yellow  */
+  [12] = "#85b78d", /* blue    */
+  [13] = "#7eb4ad", /* magenta */
+  [14] = "#8da8c7", /* cyan    */
+  [15] = "#a999cc", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"#1c1529", /* default background colour */
+  /* special colors */
+  [256] = "#1c1529", /* background */
+  [257] = "#efeaf9", /* foreground */
 };
 
 
@@ -129,10 +125,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
