4136fc80acd1e4a7f11951bd412ec63931022cb2
Zexk <bouraoui@outlook.it>
2024-12-03 18:28:27 +0100

    custom font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "ProTamSym2x:antialias=true:autohint=true";
+static char *font = "ProTamsyn2x:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
