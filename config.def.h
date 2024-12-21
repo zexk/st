e4a8ab590b378b2aacadca3ac5ee48ce0f3c7094
Zexk <bouraoui@outlook.it>
2024-12-21 20:21:02 +0100

    fontsize

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "PixelCode:antialiasing=false:size=26";
+static char *font = "PixelCode:antialiasing=false";
 static int borderpx = 1;
 
 /* disable bold, italic and roman fonts globally */
